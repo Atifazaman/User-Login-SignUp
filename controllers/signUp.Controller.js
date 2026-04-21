37d2c5a88fd9433215e044283a52439995f75493
Atifazaman <136952774+Atifazaman@users.noreply.github.com>
2026-04-21 13:42:09 +0530

    Update signUp.Controller.js

--- controllers/signUp.Controller.js
+++ controllers/signUp.Controller.js
@@ -21,10 +21,35 @@ const newUser = await userTable.create({
   } catch (error) {
      res.status(500).json({ error: error.message });
   }  
+}
 
+const checkUser=async(req,res)=>{
+  try {
+     const {email,password}=req.body
+ const user=await userTable.findOne({where:{
+    email
+}})
+
+if(!user){
+    return res.status(404).json({ message: "User does not exists" });
+}
+if (user.password === password) {
+    return res.status(200).json({
+        message: "Login successful",
+    });
+} else {
+    return res.status(401).json({
+        message: "User not authorized"
+    });
+}
+
+  } catch (error) {
+     res.status(500).json({ error: error.message });
+  }  
 }
 
 
+
 module.exports={
-    addUser
+    addUser,checkUser
 }
